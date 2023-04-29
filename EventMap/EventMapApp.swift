fe7a0f911b93105342627dea4b4aa93d0328adbb
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-04-29 22:38:24 +0900

    Initial Commit

---
+++ EventMap/EventMapApp.swift
@@ -0,0 +1,18 @@
+//
+//  EventMapApp.swift
+//  EventMap
+//
+//  https://github.com/RyoDeveloper/EventMap
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct EventMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
