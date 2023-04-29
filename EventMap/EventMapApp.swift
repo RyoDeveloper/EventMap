ab3bbb99decab8b51ff0cb506db50b4adba85934
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-04-30 00:23:54 +0900

    Add: Firebase Authenticationの導入

--- EventMap/EventMapApp.swift
+++ EventMap/EventMapApp.swift
@@ -7,9 +7,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct EventMapApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
